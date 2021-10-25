0e9b507295d4089d05e233b290a1eb0f5f2f408e
Cheryl Carpenter <cherylcarpenter2015@gmail.com>
2021-10-25 10:07:17 -0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>{{ data }}</h1>
+</template>
+
+<script setup>
+const { data } = await useFetch("/api/demo", {
+  defer: true,
+});
+</script>
+
+<style></style>
---
+++ server/api/demo.js
@@ -0,0 +1,11 @@
+import { $fetch } from "ohmyfetch";
+
+const apiUrl = "https://my.api.mockaroo.com/tasks.json?key=c1eb9230";
+
+export default async (req, res) => {
+  const data = await $fetch(apiUrl);
+  console.log(data);
+  return {
+    data,
+  };
+};
