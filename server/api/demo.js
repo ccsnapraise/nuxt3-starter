e720acb720ee00292a49709e5efe6234089d6676
Cheryl Carpenter <cherylcarpenter2015@gmail.com>
2021-10-25 10:22:59 -0500

    cleanup

--- server/api/demo.js
+++ server/api/demo.js
@@ -4,7 +4,7 @@ const apiUrl = "https://my.api.mockaroo.com/tasks.json?key=c1eb9230";
 
 export default async (req, res) => {
   const data = await $fetch(apiUrl);
-  console.log(data);
+  // console.log(data);
   return {
     data,
   };
